9cf7cf0e9a1abd758500d0bf61c8755a42efce84
Francisco Soto <ebobby@ebobby.org>
2013-06-27 11:32:22 -0700

    Made the quicklisp shorter.

--- google/quick-sort.lisp
+++ google/quick-sort.lisp
@@ -7,23 +7,18 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defun quick-sort (arr &optional (left 0) (right (1- (array-total-size arr))))
-  (labels ((swap (arr i j)
-             (let (tmp)
-               (setf tmp (aref arr i))
-               (setf (aref arr i) (aref arr j))
-               (setf (aref arr j) tmp)))
-           (partition (arr left right)
+  (labels ((partition (arr left right)
              (let* ((pivot (+ left (ash (- right left) -1)))
                     (pivot-value (aref arr pivot))
                     (store-index left))
-               (swap arr pivot right)
+               (rotatef (aref arr pivot) (aref arr right))
                (loop
                   for i from left to (1- right)
                   when (< (aref arr i) pivot-value)
                   do
-                    (unless (= i store-index) (swap arr i store-index))
+                    (unless (= i store-index) (rotatef (aref arr i) (aref arr store-index)))
                     (incf store-index))
-               (swap arr store-index right)
+               (rotatef (aref arr store-index) (aref arr right))
                store-index)))
     (when (< left right)
       (let ((index (partition arr left right)))

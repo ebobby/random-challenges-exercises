8905a1c49334589dc4b8cde171008e462d952293
Francisco Soto <ebobby@ebobby.org>
2013-06-26 19:57:45 -0700

    Made the merge-sort use arrays as opposed of lists, and simplified the code a bit.

--- google/merge-sort.lisp
+++ google/merge-sort.lisp
@@ -1,30 +1,31 @@
-(defun merge-sort (collection)
+(defun merge-sort (arr)
   (labels ((merge-helper (l1 l2)
-             (loop
-                with result = (list)
-                with l1-len = (length l1)
-                with l2-len = (length l2)
-                with i = 0
-                with j = 0
-                when (= (+ i j) (+ l1-len l2-len)) return (reverse result)
-                do
-                  (cond ((and (< i l1-len) (< j l2-len))
-                         (if (< (nth i l1) (nth j l2))
-                             (progn
-                               (push (nth i l1) result)
-                               (incf i))
-                             (progn
-                               (push (nth j l2) result)
-                               (incf j))))
-                        ((< i l1-len)
-                         (push (nth i l1) result)
-                         (incf i))
-                        ((< j l2-len)
-                         (push (nth j l2) result)
-                         (incf j))))))
-    (let ((len (length collection)))
+             (macrolet
+                 ((move-value (dst dst-index src src-index)
+                    `(progn
+                       (setf (aref ,dst ,dst-index) (aref ,src ,src-index))
+                       (incf ,dst-index)
+                       (incf ,src-index))))
+               (loop
+                  with l1-len = (array-total-size l1)
+                  with l2-len = (array-total-size l2)
+                  with result = (make-array (+ l1-len l2-len))
+                  with i = 0
+                  with j = 0
+                  with m = 0
+                  when (= (+ i j) (+ l1-len l2-len)) return result
+                  do
+                    (cond ((and (< i l1-len) (< j l2-len))
+                           (if (< (aref l1 i) (aref l2 j))
+                               (move-value result m l1 i)
+                               (move-value result m l2 j)))
+                          ((< i l1-len)
+                           (move-value result m l1 i))
+                          ((< j l2-len)
+                           (move-value result m l2 j)))))))
+    (let ((len (array-total-size arr)))
       (if (<= len 1)
-          collection
+          arr
           (let ((middle (ash len -1)))
-            (merge-helper (merge-sort (subseq collection 0 middle))
-                          (merge-sort (subseq collection middle len))))))))
+            (merge-helper (merge-sort (subseq arr 0 middle))
+                          (merge-sort (subseq arr middle len))))))))

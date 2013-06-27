807f4fccc31c92d629952ce8b7db45c53bc1fa23
Francisco Soto <fms@cordelia.local>
2013-06-26 18:11:35 -0700

    Added a not so fast fastmemcpy that aligns pointers to do the mass copy.

---
+++ google/memcpy.c
@@ -0,0 +1,138 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+void naivememcpy (void *dst, void *src, size_t n) {
+    uint8_t *d =(uint8_t *)dst, *s = (uint8_t *)src;
+    int i;
+
+    for (i = 0; i < n; i++) {
+        *(d++) = *(s++);
+    }
+}
+
+void fastmemcpy_read_byte (uint64_t *constructed, int *read, uint8_t **src) {
+    *constructed += (**src & 0xFF) << (*read << 3);
+    *read        += 1;
+    *src         += 1;
+}
+
+void fastmemcpy_read_dword (uint64_t *constructed, int *read, uint64_t **src) {
+    *constructed = **src;
+    *read        = 8;
+    *src        += 1;
+}
+
+void fastmemcpy (void *dst, void *src, size_t n) {
+    uint8_t *byte_d =(uint8_t *)dst, *byte_s = (uint8_t *)src, byte;
+    uint64_t constructed = 0, buffer = 0, *dword_d, *dword_s;
+    int i, j, read = 0, buffered = 0, written = 0;
+    long tmp;
+
+    if (n < 8) {
+        for (i = 0; i < n; i++) {
+            *(byte_d++) = *(byte_s++);
+            written++;
+        }
+    }
+    else {
+        tmp = ((long) byte_s) & 0x7;
+        for (i = 0; i < tmp; i++) {
+            fastmemcpy_read_byte(&constructed, &read, &byte_s);
+            n--;
+        }
+        dword_s = (uint64_t *)byte_s; // now read pointer is aligned.
+
+        tmp = ((long) byte_d) & 0x7;
+        for (i = 0; i < tmp; i++) {
+            if (read == 0) {
+                if (n < 8) {
+                    tmp = n;
+                    for (i = 0; i < tmp; i++) {
+                        fastmemcpy_read_byte(&constructed, &read, &byte_s);
+                        n--;
+                    }
+                    dword_s = (uint64_t *)byte_s;
+                }
+                else {
+                    fastmemcpy_read_dword(&constructed, &read, &dword_s);
+                    n -= 8;
+                }
+            }
+
+            byte = constructed & 0xFF;
+            constructed = constructed >> 8;
+            read--;
+            *(byte_d++) = byte;
+            written += 1;
+        }
+        dword_d = (uint64_t *)byte_d; // write pointer is aligned.
+
+        while (n >= 8) {
+            if (read < 8) {
+                tmp = 8 - read;
+                for (j = 0; j < tmp; j++) {
+                    if (buffered == 0) {
+                        fastmemcpy_read_dword(&buffer, &buffered, &dword_s);
+                        n -= 8;
+                    }
+
+                    constructed += (buffer & 0xFF) << (read << 3);
+                    buffer = buffer >> 8;
+                    read++;
+                    buffered--;
+                }
+            }
+            *(dword_d++) = constructed;
+            written += 8;
+            constructed = 0;
+            read = 0;
+        }
+
+        byte_s = (uint8_t *)dword_s;
+        byte_d = (uint8_t *)dword_d;
+
+        tmp = read;
+        for (i = 0; i < tmp; i++) {
+            *(byte_d++) = constructed & 0xFF;
+            constructed = constructed >> 8;
+            read--;
+            written += 1;
+        }
+
+        tmp = buffered;
+        for (i = 0; i < tmp; i++) {
+            *(byte_d++) = buffer & 0xFF;
+            buffer = buffer >> 8;
+            written += 1;
+            buffered--;
+        }
+
+        for (i = 0; i < n; i++) {
+            *(byte_d++) = *(byte_s++);
+            written += 1;
+        }
+    }
+}
+
+typedef int bool;
+
+#define HOWMANY 412345
+#define TRUE  1
+#define FALSE 0
+
+int main (void) {
+    int i = 0, j = 0;
+    unsigned long stuff1[HOWMANY] = {  };
+    unsigned long stuff2[HOWMANY] = {  };
+    bool equals = TRUE;
+
+    for (i = 0; i < 5000; i++) {
+        stuff1[i] = i + 1;
+    }
+    for (j = 0; j < 1000; j++) {
+        fastmemcpy(&stuff2, &stuff1, sizeof(stuff1));
+    }
+
+    return 0;
+}

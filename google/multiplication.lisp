e353dfcd6bbecbac53e89e6a2b398efc373f9d89
Francisco Soto <ebobby@ebobby.org>
2013-07-06 18:17:38 -0700

    Added the shift and add algorithm for multiplying two numbers together.

--- google/multiplication.lisp
+++ google/multiplication.lisp
@@ -6,10 +6,21 @@
 ;; Francisco Soto <ebobby@ebobby.org>
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defun multiply (multiplier multiplicand)
+(defun multiply-peasant (multiplier multiplicand)
   (if (zerop multiplier)
       0
       (+ (if (plusp (logand multiplier 1))
              multiplicand
              0)
          (multiply (ash multiplier -1) (ash multiplicand 1)))))
+
+(defun multiply-shift-add (multiplier multiplicand)
+  (loop
+     with accum = 0
+     for i = 0 then (1+ i)
+     while (plusp multiplier)
+     do
+       (when (plusp (logand multiplier 1))
+         (incf accum (ash multiplicand i)))
+       (setf multiplier (ash multiplier -1))
+     finally (return accum)))

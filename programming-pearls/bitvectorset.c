77ead18b3e6060ca261b8a425d1c2531c8365f8f
Francisco Soto <ebobby@ebobby.org>
2013-07-18 02:15:58 -0700

    Added a bit vector based set.

---
+++ programming-pearls/bitvectorset.c
@@ -0,0 +1,50 @@
+/******************************************************************************************************************************************************
+ * Set bitmap structure
+ *
+ * Exercise 1.2 from Programming Pearls
+ *
+ * Francisco Soto <ebobby@ebobby.org>
+ ******************************************************************************************************************************************************/
+
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <time.h>
+#include "bitvectorset.h"
+
+#define SET_SIZE 25000000
+
+int main (void) {
+    bit_vector_set *set = bit_vector_set_create(SET_SIZE);
+    unsigned long i;
+
+    if (set == NULL) {
+        printf("No memory for set.\n");
+        return -1;
+    }
+
+    srand(time(NULL));
+
+    for (i = 0; i < rand()%10000; i++)
+        bit_vector_set_add(set, rand()%SET_SIZE);
+
+    for (i = 1; i <= SET_SIZE; i++) {
+        if (bit_vector_set_is_element_of(set, i)) {
+            printf("%lu is element of the set.\n", i);
+            bit_vector_set_remove(set, i);
+        }
+    }
+    printf("\nShould be empty now:");
+
+    for (i = 1; i <= SET_SIZE; i++) {
+        if (bit_vector_set_is_element_of(set, i)) {
+            printf("%lu is element of the set.\n", i);
+            bit_vector_set_remove(set, i);
+        }
+    }
+    printf("\n");
+
+    bit_vector_set_destroy(set);
+
+    return 0;
+}
---
+++ programming-pearls/bitvectorset.h
@@ -0,0 +1,71 @@
+/******************************************************************************************************************************************************
+ * Set bitmap structure
+ *
+ * Exercise 1.2 from Programming Pearls
+ *
+ * Francisco Soto <ebobby@ebobby.org>
+ ******************************************************************************************************************************************************/
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+
+#define BIT_VECTOR_SET_BITS_PER_ELEMENT    32
+#define BIT_VECTOR_SET_SHIFT               5
+#define BIT_VECTOR_SET_POSITION(EL)        (EL >> 5)
+#define BIT_VECTOR_SET_SET_BIT_MASK(EL)    (1 << (EL & 0x1F))
+#define BIT_VECTOR_SET_UNSET_BIT_MASK(EL)  ((1 << (EL & 0x1F)) ^ 0xFFFFFFFF)
+
+typedef struct {
+    uint32_t *bitmap;  // bit vector
+    unsigned long n;    // number of elements in the set universe
+} bit_vector_set;
+
+bit_vector_set *bit_vector_set_create (unsigned long elements) {
+    bit_vector_set *set = (bit_vector_set *)malloc(sizeof(bit_vector_set));
+    int size;
+
+    if (set == NULL)
+        return NULL;
+
+    size = sizeof(uint32_t) * (BIT_VECTOR_SET_POSITION(elements) + 1);
+    set->bitmap = (uint32_t *)malloc(size);
+    memset(set->bitmap, 0, size);
+
+    if (set->bitmap == NULL) {
+        free(set);
+        return NULL;
+    }
+
+    set->n = elements;
+
+    return set;
+}
+
+void bit_vector_set_destroy (bit_vector_set *set) {
+    if (set == NULL)
+        return;
+    free(set->bitmap);
+    free(set);
+}
+
+void bit_vector_set_add (bit_vector_set *set, unsigned long element) {
+    if (element > set->n)
+        return;
+    element--;
+    set->bitmap[BIT_VECTOR_SET_POSITION(element)] |= BIT_VECTOR_SET_SET_BIT_MASK(element);
+}
+
+void bit_vector_set_remove (bit_vector_set *set, unsigned long element) {
+    if (element > set->n)
+        return;
+    element--;
+    set->bitmap[BIT_VECTOR_SET_POSITION(element)] &= BIT_VECTOR_SET_UNSET_BIT_MASK(element);
+}
+
+int bit_vector_set_is_element_of (bit_vector_set *set, unsigned long element) {
+    if (element > set->n)
+        return 0;
+    element--;
+    return set->bitmap[BIT_VECTOR_SET_POSITION(element)] & BIT_VECTOR_SET_SET_BIT_MASK(element);
+}
